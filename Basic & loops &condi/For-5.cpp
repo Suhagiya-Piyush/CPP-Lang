07e7735a603f7fd85ba55334dfb2c08cb4046767
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2023-06-24 19:29:49 +0530

    24-6-23

---
+++ Basic & loops &condi/For-5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j, n=2;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            cout<<" "<<n;
+        }cout<<"\n";
+        n+=2;
+    }
+}
---
+++ Basic & loops &condi/For-8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j;
+    char n='A';
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            cout<<" "<<n;
+            n++;
+        }cout<<"\n";
+    }
+}
